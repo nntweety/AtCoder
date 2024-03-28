aaa68867e065b777f42e5352ab28b8fcda0f9086
nntweety <z3uy8nce08jrdbs1dyq0@docomo.ne.jp>
2024-03-28 22:54:58 +0900

    132A

--- AtCoder Beginner Contest/132/A. Fifty-Fifty100.py
+++ AtCoder Beginner Contest/132/A. Fifty-Fifty100.py
@@ -1,1 +1,35 @@
 S = input()
+
+def check(i):
+    return i == 4
+
+def checkHalf(str):
+  flg = True
+
+  if not check(len(str)):
+    flg = False
+    return flg
+
+  firstStr = S[0]
+  secondStr = ''
+  strNum = 0
+
+  for i in range(len(str)):
+    if str[i] == firstStr:
+      strNum += 1
+    if S[i] != firstStr and not secondStr:
+        secondStr = S[i]
+    if secondStr != '':
+      if S[i] != firstStr and S[i] != secondStr:
+        flg = False
+        return flg
+
+  if(strNum != (len(S) / 2)):
+    flg = False
+
+  return flg
+
+if checkHalf(S):
+  print('Yes')
+else:
+  print('No')
